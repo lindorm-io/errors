059c4db5632a9f8510856e3556025f9226cfc1ef
Jonn Nilsson <jonn@jonn.nu>
2021-06-24 13:20:01 +0200

    fix: add data to redirect error

--- src/error/RedirectError.test.ts
+++ src/error/RedirectError.test.ts
@@ -3,25 +3,41 @@ import { RedirectError } from "./RedirectError";
 import { LindormError } from "./LindormError";
 
 describe("RedirectError", () => {
+  const options = {
+    code: "code",
+    description: "description",
+    redirect: "redirect",
+    state: "state",
+    uri: "uri",
+  };
+
   describe("instanceOf", () => {
     test("should be an Error", () => {
-      expect(new RedirectError("message", { redirect: "redirect" })).toStrictEqual(expect.any(Error));
+      expect(new RedirectError("message", options)).toStrictEqual(expect.any(Error));
     });
 
     test("should be an ExtendableError", () => {
-      expect(new RedirectError("message", { redirect: "redirect" })).toStrictEqual(expect.any(ExtendableError));
+      expect(new RedirectError("message", options)).toStrictEqual(expect.any(ExtendableError));
     });
 
     test("should be an LindormError", () => {
-      expect(new RedirectError("message", { redirect: "redirect" })).toStrictEqual(expect.any(LindormError));
+      expect(new RedirectError("message", options)).toStrictEqual(expect.any(LindormError));
     });
 
     test("should be a RedirectError", () => {
-      expect(new RedirectError("message", { redirect: "redirect" }).name).toBe("RedirectError");
+      expect(new RedirectError("message", options).name).toBe("RedirectError");
     });
 
-    test("should contain redirect string", () => {
-      expect(new RedirectError("message", { redirect: "redirect" }).redirect).toBe("redirect");
+    test("should set options", () => {
+      expect(new RedirectError("message", options)).toStrictEqual(
+        expect.objectContaining({
+          code: "code",
+          description: "description",
+          redirect: "redirect",
+          state: "state",
+          uri: "uri",
+        }),
+      );
     });
   });
 });
--- src/error/RedirectError.ts
+++ src/error/RedirectError.ts
@@ -2,15 +2,27 @@ import { LindormError } from "./LindormError";
 import { ExtendableErrorOptions } from "./ExtendableError";
 
 interface Options extends ExtendableErrorOptions {
+  code: string;
+  description: string;
   redirect: string;
+  state?: string;
+  uri?: string;
 }
 
 export class RedirectError extends LindormError {
+  public readonly code: string;
+  public readonly description: string;
   public readonly redirect: string;
+  public readonly state: string | null;
+  public readonly uri: string | null;
 
   public constructor(message: string, options: Options) {
     super(message, options);
 
+    this.code = options.code;
+    this.description = options.description;
     this.redirect = options.redirect;
+    this.state = options.state || null;
+    this.uri = options.uri || null;
   }
 }
